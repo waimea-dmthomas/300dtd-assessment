58e9035ab574f85a714827c35c04630df2b65654
waimea-dmthomas <dmthomas@waimea.school.nz>
2024-06-21 12:37:46 +1200

    Updated .htaccess

--- site/index.php
+++ site/index.php
@@ -8,8 +8,21 @@ require_once 'lib/router.php';
 
 //-------------------------------------------------------------
 // Site Configuration
-const SITE_NAME  = 'PHP Routing with HTMX';
+const SITE_NAME  = 'PBS Forums';
 const SITE_OWNER = 'Waimea College';
+
+
+//-------------------------------------------------------------
+// Setup a session
+session_name('PBSFORUMS');
+session_start();
+
+
+//-------------------------------------------------------------
+// Check which type of user we are
+$userName   = $_SESSION['user']['username']     ?? 'Guest';
+$isLoggedIn = $_SESSION['user']['loggedIn']     ?? false;
+$isAdmin    = $_SESSION['user']['admin']        ?? false;
 
 
 //-------------------------------------------------------------
